944e6b2379d6a4c8fb59970b5f60b579a84d044e
Aayushi5899 <W0787383@myscc.ca>
2022-12-14 20:21:04 -0500

    Create the pages home and profile

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+/* import {DatePicker} from 'antd';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> <DatePicker /> </div>
+    
+  );
+}
+
+export default App;
+ */
+
+import React, { useState, useEffect } from "react";
+import { Row, Col, Divider, Typography, Space } from "antd";
+import axios from "axios";
+import MyCard from "./Components/MyCard";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import {FaHome} from "react-icons/fa";
+
+const { Title } = Typography;
+
+function App() {
+  const [blogs, setBlogs] = useState(null);
+  const [status, setStatus] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setBlogs(response.data);
+        setStatus(true);
+      })
+      .catch(() => {
+        console.log("API call Failed...!!!");
+        setStatus(false);
+      });
+  }, []);
+
+  return (
+    <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+      <Row gutter={16}>
+        <Col className="gutter-row">
+          <Title>Blog Posting App</Title>
+        </Col>
+      </Row>
+      <Divider orientation="center">Items</Divider>
+
+      <Row gutter={16}>
+        {status &&
+          blogs.map((blogObj) => {
+            return (
+              <MyCard
+                key={blogObj.id}
+                title={blogObj.title}
+                content={blogObj.body}
+              />
+            );
+          })}
+      </Row>
+      <Divider orientation="center">Navigation</Divider>
+
+      <Row>
+        <nav>
+          <Link to="/home"><FaHome /> Home </Link> |{" "}
+          <Link to="/profile">Profile </Link>
+        </nav>
+      </Row>
+      <Divider orientation="center">Page</Divider>
+      <Row>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Row>
+    </Space>
   );
 }
 
